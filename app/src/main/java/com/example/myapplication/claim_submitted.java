5a266c9d9c29fb8cecb88343feb366687681693a
Lea Cerron <15lc50@queensu.ca>
2019-05-08 15:26:39 -0400

    back button from claim submitted test

--- app/src/main/java/com/example/myapplication/claim_submitted.java
+++ app/src/main/java/com/example/myapplication/claim_submitted.java
@@ -1,11 +1,13 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class claim_submitted extends AppCompatActivity {
 
@@ -15,13 +17,18 @@ public class claim_submitted extends AppCompatActivity {
         setContentView(R.layout.activity_claim_submitted);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        configureBackButton();
+    }
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+    //code for configuring the back button
+    //back button returns to submit_claim page
+    private void configureBackButton(){
+        Button backButton = (Button) findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View view){
+                finish();
+                //startActivity(new Intent(claim_submitted.this, submit_claim.class));
             }
         });
     }
